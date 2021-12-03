0a4be0c4fd95812c6e6e34dd8efe068cfa93814c
shorifnahid <naimulnihal2696@gmail.com>
2021-12-04 00:36:18 +0600

    1st upload

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(function(){
+    //slick slider
+    $('.slick_slider'). slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows:false,
+        dots:true,
+        dotsClass:'slider_dots',
+    })
+})
